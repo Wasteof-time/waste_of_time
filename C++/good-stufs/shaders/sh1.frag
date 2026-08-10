6a72ef720199be080fcfdd292579d46ef8c0ca8a
JCKawin <jckawin2007@gmail.com>
2026-08-11 01:04:18 +0530

    playing with some shaders

--- C++/good-stufs/shaders/sh1.frag
+++ C++/good-stufs/shaders/sh1.frag
@@ -1,7 +1,13 @@
 #version 330 core
 
 out vec4 FragColor;
+uniform float u_time;
+
+in vec4 vertex_color;
 
 void main() {
-    FragColor = vec4(1.0 , 1.0 , 1.0 , 1.0);
+    float green = sin(u_time) * 0.5 + 0.5;
+    float red = sin(u_time + 90) * 0.5 + 0.5;
+    float blue = cos(u_time) * 0.5 + 0.5;
+    FragColor = vec4(red * (vertex_color.x + 0.5), green * (vertex_color.y + 0.5), blue * (vertex_color.x + 0.5), 1.0);
 }
--- C++/good-stufs/src/main.cpp
+++ C++/good-stufs/src/main.cpp
@@ -4,7 +4,7 @@
 #include <fstream>
 #include <sstream>
 
-std::string shader_file_import(const std::string& file_path) {
+static std::string shader_file_import(const std::string& file_path) {
     std::ifstream file(file_path, std::ios::in | std::ios::binary);
     if (!file.is_open()) {
         std::cerr << "Failed to open shader: " << file_path << std::endl;
@@ -36,10 +36,18 @@ int main() {
 
     // our vertices
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+       -0.5f, -0.5f, 0.0f,  // bottom left
+       -0.5f,  0.5f, 0.0f   // top left
     };
+
+    unsigned int indicies[] = {
+        0 , 1 , 3, // triangle one
+        1 , 2 , 3  // triangle two
+    };
+
+
 
     // vertex shader import
     std::string vertex_shader_string = shader_file_import("../shaders/sh1.vert");
@@ -60,7 +68,13 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER , VBO);
     glBufferData(GL_ARRAY_BUFFER , sizeof(vertices) , vertices , GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0 , 3 , GL_FLOAT , GL_FALSE, 3* sizeof(float) , nullptr);
+    // EBO
+    unsigned int EBO;
+    glGenBuffers(1 , &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER , EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER , sizeof(indicies) , indicies , GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0 , 4 , GL_FLOAT , GL_FALSE, 3 * sizeof(float) , nullptr);
     glEnableVertexAttribArray(0);
     glBindVertexArray(0);
 
@@ -68,15 +82,14 @@ int main() {
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 
-    glShaderSource(vertexShader , 1 , &vertex_shader ,NULL);
-    glShaderSource(fragmentShader , 1 , &fragment_shader , NULL);
+    glShaderSource(vertexShader , 1 , &vertex_shader ,nullptr);
+    glShaderSource(fragmentShader , 1 , &fragment_shader , nullptr);
     glCompileShader(vertexShader);
     glCompileShader(fragmentShader);
 
 
     // creating shader program
-    unsigned int shaderProgram ;
-    shaderProgram = glCreateProgram();
+    unsigned int shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram , vertexShader);
     glAttachShader(shaderProgram , fragmentShader);
     glLinkProgram(shaderProgram);
@@ -84,18 +97,26 @@ int main() {
 
 
 
+    //time stuffs
+    GLint timeLoc = glGetUniformLocation(shaderProgram, "u_time");
 
 
-
-
+    int nrAttributes;
+    glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+    std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
 
     while (!glfwWindowShouldClose(window)) {
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES , 0 , 3);
+        auto currentTime = static_cast<float> (glfwGetTime());
+        glUniform1f(timeLoc, currentTime);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
 
         glfwSwapBuffers(window);
         glfwPollEvents();
+        
     }
 
 
